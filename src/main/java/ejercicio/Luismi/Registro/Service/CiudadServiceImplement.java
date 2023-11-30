a0450f941a82b5dad26bc11f717d7012de6db87a
mainluismi <luismisc02@gmail.com>
2023-11-30 17:01:59 +0100

    Arreglo fallo en la clase service

--- src/main/java/ejercicio/Luismi/Registro/Service/CiudadServiceImplement.java
+++ src/main/java/ejercicio/Luismi/Registro/Service/CiudadServiceImplement.java
@@ -3,9 +3,11 @@ package ejercicio.Luismi.Registro.Service;
 import ejercicio.Luismi.Registro.Entity.Ciudad;
 import ejercicio.Luismi.Registro.Repository.CiudadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CiudadServiceImplement implements CiudadService{
     @Autowired
     CiudadRepository ciudadRepository;
